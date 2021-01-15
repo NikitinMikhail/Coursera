b1a9d392d784da13500b10a7ac3bb7b240b5f188
Mikhail Nikitin <mikhail.nikitin@phystech.edu>
2021-01-15 11:59:57 +0300

    .idea added again

---
+++ Week4/magic_methods.py
@@ -0,0 +1,16 @@
+import tempfile
+import os
+
+
+class File():
+    def __init__(self, path_to_file=None):
+        self.path_to_file = path_to_file
+
+    def __add__(self, other):
+        pass
+    
+    def read(self):
+        pass
+
+    def write(self):
+        pass
